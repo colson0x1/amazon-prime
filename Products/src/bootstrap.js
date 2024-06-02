b0391fbb20abb80ee7c58d6e15f5d462e01c59eb
Colson <colsoncalifornication@gmail.com>
2024-06-02 13:38:46 +0545

    sub app/microfrontend execution context: develop each sub projects  in isolation and run each project in the context of the Container Microfrontends - approach: refactor bootstrap

--- Products/src/bootstrap.js
+++ Products/src/bootstrap.js
@@ -10,3 +10,15 @@ for (let i = 0; i < 9; i++) {
 // console.log(products);
 
 document.querySelector('#dev-products').innerHTML = products;
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file which DEFINITELY has an element
+// with an id of 'dev-products'
+// We want to immediately render our app into that element
+
+// Context/Situation #2
+// We are running this file in development or production through the CONTAINER
+// app
+// No GUARANTEE that an element with an id of 'dev-products' exists
+// WE DO NOT WANT to try to immediately render the app
